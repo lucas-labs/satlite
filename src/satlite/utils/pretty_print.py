f8e4de4d3b1d8cfddf077947499988e5da216996
Lucas Colombo <lucasncolombo@gmail.com>
2025-05-11 14:31:29 -0300

    feat: ✨ change color for field names in pretty_repr to blue

--- src/satlite/utils/pretty_print.py
+++ src/satlite/utils/pretty_print.py
@@ -43,13 +43,13 @@ def pretty_repr(obj: Any, indent: int = 0, top_level: bool = True) -> str:
         for f in fields(obj):
             value = getattr(obj, f.name)
             if is_dataclass(value):
-                lines.append('  ' * (indent + 1) + f'- {c("magenta")(f.name)}')
+                lines.append('  ' * (indent + 1) + f'- {c("blue")(f.name)}')
                 lines.append(pretty_repr(value, indent + 2, top_level=False))
             elif isinstance(value, dict):
-                lines.append('  ' * (indent + 1) + f'- {c("magenta")(f.name)}:')
+                lines.append('  ' * (indent + 1) + f'- {c("blue")(f.name)}:')
                 lines.append(pretty_repr_dict(value, indent + 2))
             elif isinstance(value, (list, tuple, set)):
-                lines.append('  ' * (indent + 1) + f'- {c("magenta")(f.name)}:')
+                lines.append('  ' * (indent + 1) + f'- {c("blue")(f.name)}:')
                 lines.append(pretty_repr_iterable(value, indent + 2))
             else:
                 lines.append('  ' * (indent + 1) + f'- {c("blue")(f.name)}: {value}')
